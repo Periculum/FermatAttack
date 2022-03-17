566ef6ce8359933858156cb5a1e292919d5d4775
Wilhelm Drehling <wilhelmdrehling@web.de>
2022-03-17 16:46:09 +0100

    Small Changes
    Changed if-statement and added bigger Module (needs now 22 Tries to find the Primes).

--- FermatAttack.py
+++ FermatAttack.py
@@ -1,26 +1,28 @@
-# Fermat-Attack on weak RSA-Moduls (Prime Number are to close)
+# Fermat-Attack on weak RSA-Moduls
 import math
 
 # two close Prime Number (just an Example)
-MODUL = 19 * 91
+MODUL = 18487289 * 18543067
 
 
+# recursive Method that finds the distance between the mid and primes
 def attack(guess, tries):
     r = pow(guess + tries, 2) - MODUL
-    if(math.sqrt(r).is_integer() != True):
+    if not math.sqrt(r).is_integer():
         tries += 1
         return attack(guess, tries)
     else:
-        return (guess + tries), math.sqrt(r)
+        return (guess + tries), math.sqrt(r), tries + 1
 
 
 def main():
     first_guess = round(math.sqrt(MODUL), 0)
-    b, r = attack(first_guess, 0)
+    b, r, tries = attack(first_guess, 0)
     prime1 = b - r
     prime2 = b + r
-    print("The Primes are {:d} and {:d}.".format(int(prime1), int(prime2)))
-    print(prime1 * prime2)
+    print("It took {:d} Tries to find the Primes {:d} and {:d}.".format(
+        tries, int(prime1), int(prime2)))
+    # print(prime1 * prime2)
 
 
 if __name__ == "__main__":
