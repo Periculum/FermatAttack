87fe02bffcaeb9776723ce34b2013acdbb9bff20
Wilhelm Drehling <wilhelmdrehling@web.de>
2022-04-22 22:46:00 +0200

    Changed Names
    Changed Names: r to b, b to a. Small Changes

--- FermatAttack.py
+++ FermatAttack.py
@@ -7,22 +7,21 @@ MODUL = 18487289 * 18543067
 
 # recursive Method that finds the distance between the mid and primes
 def attack(guess, tries):
-    r = pow(guess + tries, 2) - MODUL
-    if not math.sqrt(r).is_integer():
+    b = pow(guess + tries, 2) - MODUL
+    if not math.sqrt(b).is_integer():
         tries += 1
         return attack(guess, tries)
     else:
-        return (guess + tries), math.sqrt(r), tries + 1
+        return (guess + tries), math.sqrt(b), tries + 1
 
 
 def main():
     first_guess = round(math.sqrt(MODUL), 0)
-    b, r, tries = attack(first_guess, 0)
-    prime1 = b - r
-    prime2 = b + r
+    a, b, tries = attack(first_guess, 0)
+    prime1 = a - b
+    prime2 = a + b
     print("It took {:d} Tries to find the Primes {:d} and {:d}.".format(
         tries, int(prime1), int(prime2)))
-    # print(prime1 * prime2)
 
 
 if __name__ == "__main__":
