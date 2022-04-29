61d779985f30cbbd46c781ba6c48ef490ebe5797
Wilhelm Drehling <wilhelmdrehling@web.de>
2022-04-29 16:33:01 +0200

    Changed recursive method to iterative
    Small additional changes, including sqrt() to isqrt(). Program handles now bigger numbers than before

--- FermatAttack.py
+++ FermatAttack.py
@@ -5,23 +5,25 @@ import math
 MODUL = 18487289 * 18543067
 
 
-# recursive Method that finds the distance between the mid and primes
-def attack(guess, tries):
+# Method that finds the distance between the mid and primes
+def attack(guess, max_tries=1000000):
+  for tries in range(max_tries):
     b = pow(guess + tries, 2) - MODUL
-    if b < 0 or not math.sqrt(b).is_integer():
-        tries += 1
-        return attack(guess, tries)
-    else:
-        return (guess + tries), math.sqrt(b), tries + 1
+    if b >= 0 and pow(math.isqrt(b), 2) == b:
+      return guess + tries, math.isqrt(b), tries + 1
+  return 0, 0, max_tries
 
 
 def main():
-    first_guess = round(math.sqrt(MODUL), 0)
-    a, b, tries = attack(first_guess, 0)
-    prime1 = a - b
-    prime2 = a + b
-    print("It took {:d} Tries to find the Primes {:d} and {:d}.".format(
-        tries, int(prime1), int(prime2)))
+  first_try = math.isqrt(MODUL)
+  a, b, tries = attack(first_try)
+  prime1 = a - b
+  prime2 = a + b
+  if a != 0:
+    print(
+        f"Es braucht {tries} Versuche, um die Faktoren {prime1} und {prime2} zu finden.")
+  else:
+    print(f"Kein Treffer in {tries} Versuchen.")
 
 
 if __name__ == "__main__":
